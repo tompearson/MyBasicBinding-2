955a8a1fcdad3f4413d41fbf61b05fe420a76552
Tom Pearson <tom_pearson@yahoo.com>
2022-04-06 14:29:00 -0700

    initial commit Working well. Updated    implementation 'androidx.lifecycle:lifecycle-extensions:2.2.0'// added     implementation 'androidx.fragment:fragment-ktx:1.4.1'// Needed for by viewModels()     Updated some comments     Fix a reverse boolean bug in MainActivity.ky     if (it == false)     Added signing config for release build     Added lifeCycle messages     Added Logs

--- app/src/main/java/com/tappyplay/mybasicbinding_2/MainActivity.kt
+++ app/src/main/java/com/tappyplay/mybasicbinding_2/MainActivity.kt
@@ -1,24 +1,50 @@
 package com.tappyplay.mybasicbinding_2
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ContentInfoCompat
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import com.tappyplay.mybasicbinding_2.databinding.ActivityMainBinding
 import viewmodels.NotesViewModel
 
 //import viewmodels.NotesViewModelFactory -- replaced with 'import viewmodels.NotesViewModel provided by dependency - androidx.activity:activity-ktx'
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel: NotesViewModel
     private lateinit var databinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: starts")
+
         databinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //
+        // Code is in onResume()
+        //
+    }
+
+    //    Android Lifecycle overrides
+    override fun onStop() {
+        super.onStop()
+//        Toast.makeText(this, getString(R.string.onStop), Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+//        Toast.makeText(this, getString(R.string.onRestart), Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "onRestart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume()")
+//        Toast.makeText(this, getString(R.string.onResume), Toast.LENGTH_SHORT).show()
+
+//        databinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val viewModel: NotesViewModel by viewModels() // provided by dependency - androidx.activity:activity-ktx
 
@@ -26,13 +52,30 @@ class MainActivity : AppCompatActivity() {
 
         databinding.lifecycleOwner = this
 
-        viewModel.list.observe(this, Observer {
-            databinding.content.text = it.toString()
-        })
-        viewModel.isStringEmpty.observe(this, Observer {
+        viewModel.list.observe(this) { databinding.content.text = it.toString() }
+
+        viewModel.isStringEmpty.observe(this) {
             if (it == false) {
-                Toast.makeText(this, "No Notes Detected", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "No Notes Detected", Toast.LENGTH_LONG).show()
             }
-        })
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart()")
+//        Toast.makeText(this, getString(R.string.onStart), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+//        Toast.makeText(this, getString(R.string.onPause), Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "onPause()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+//        Toast.makeText(this, getString(R.string.onDestroy), Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "onDestroy()")
     }
 }
--- app/src/main/java/viewmodels/MainViewModel.kt
+++ app/src/main/java/viewmodels/MainViewModel.kt
@@ -1,10 +1,16 @@
 package viewmodels
 
+import android.util.Log
 import androidx.databinding.Bindable
 import androidx.databinding.Observable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import model.NoteItem
+import java.util.*
+import kotlin.collections.ArrayList
+
+private const val TAG = "NotesViewModel"
+
 class NotesViewModel : ViewModel(), Observable {
     val isStringEmpty = MutableLiveData<Boolean>()
     @Bindable
@@ -15,25 +21,30 @@ class NotesViewModel : ViewModel(), Observable {
     private val arraylst = ArrayList<NoteItem>()
 
     init {
+        Log.d(TAG, "NotesViewModel: init")
         isStringEmpty.value = false
     }
 
     fun addData() {
+        Log.d(TAG, "NotesViewModel: addData()")
+
         val title = inputTitle.value!!
         val description = inputDescription.value!!
         if(title.isBlank()|| description.isBlank()){
             isStringEmpty.value = true
         }else{
-            inputTitle.value = " "
-            inputDescription.value = " "
-            var noteItem =NoteItem(title, description)
-            arraylst.add(noteItem)
-            list.value = arraylst
+            inputTitle.value = " "       // Clear the inputTitle field in the layout
+            inputDescription.value = " " // Clear the inputDescription field in the layout
+            var noteItem =NoteItem(title, description) // get title and description from the layout into a noteItem
+            arraylst.add(noteItem) // add the noteItem to the arraylst
+            list.value = arraylst // Put the arrayLst in the list
         }
 
     }
 
     fun clearData(){
+        Log.d(TAG, "NotesViewModel: clearData()")
+
         arraylst.clear()
         list.value = arraylst
     }
